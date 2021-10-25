3029772a065e89a25c7892b1a4cd1271e2c71346
Ross Keenan <rossk29@gmail.com>
2021-10-25 15:34:41 +0200

    refactor: :recycle: Change `Run` command name

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import type { Query } from "src/interfaces";
 
-export const cmdRunId = (q: Query) => `AC-Run: ${q.name} -> ${q.query}`;
-export const cmdRunName = (q: Query) => `Run: ${q.name} → ${q.query}`;
+export const cmdRunId = (q: Query) => `AC-All: ${q.name} -> ${q.query}`;
+export const cmdRunName = (q: Query) => `All: ${q.name} → ${q.query}`;
 
 export const cmdNextId = (q: Query) => `AC-Next: ${q.name} -> ${q.query}`;
 export const cmdNextName = (q: Query) => `Next: ${q.name} → ${q.query}`;
